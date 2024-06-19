aa4af4b29fe58c42b9e4d7c545be87e9bbd115f9
Leonardo Pelonero <leo.pelonero@hotmail.it>
2024-06-19 17:16:49 +0200

    test

--- src/project.py
+++ src/project.py
@@ -25,6 +25,7 @@ class Project(metaclass=SingletonMeta):
 
     def load_project(self):
         self.doc = Metashape.app.document
+        print("--DEBUG tipo self.doc",type(self.doc))
         self.doc.open(path=self.project_path, read_only=False)
         self.chunk = self.doc.chunk
         print("--Load Project", self.doc.path)
