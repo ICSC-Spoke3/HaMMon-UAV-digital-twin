e5be4e2e39edbec260c0d361742fda150a133122
Leonardo Pelonero <leo.pelonero@hotmail.it>
2024-06-22 13:24:31 +0200

    test

--- src/geographic_projection.py
+++ src/geographic_projection.py
@@ -95,31 +95,29 @@ class GeographicProjection:
     def exportPointCloud(self, progress_printer: str, path: str, **kwargs) -> None:
         if self.project.chunk.point_cloud:
             default_params = {
-                """
                 "source_data": Metashape.DataSource.PointCloudData,
-                "save_point_color": True,
-                "save_point_normal": True, 
-                "save_point_intensity": True,
-                "save_point_classification": True, 
-                "save_point_confidence": True,
-                "save_point_source_id": True,
-                "save_point_index": True,
-                "format": Metashape.PointCloudFormat.PointCloudFormatLAS,
-                # format Metashape.PointCloudFormat.Cesium
-                "image_format": Metashape.ImageFormat.ImageFormatTIFF,
-                "split_in_blocks": False,
-                #"classes" (list[int]) – List of point classes to be exported,
-                "save_images": True,
-                # "compression": True,               
-                # "tileset_version": "1.1",
-                # "screen_space_error" (float) – Target screen space error (Cesium format only).
-                # "folder_depth" (int) – Tileset subdivision depth (Cesium format only)
-                "subdivide_task": True
-                """
+                #"save_point_color": True,
+                #"save_point_normal": True, 
+                #"save_point_intensity": True,
+                #"save_point_classification": True, 
+                #"save_point_confidence": True,
+                #"save_point_source_id": True,
+                #"save_point_index": True,
+                #"format": Metashape.PointCloudFormat.PointCloudFormatLAS,
+                        # format Metashape.PointCloudFormat.Cesium
+                #"image_format": Metashape.ImageFormat.ImageFormatTIFF,
+                #"split_in_blocks": False,
+                            #"classes" (list[int]) – List of point classes to be exported,
+                #"save_images": True,
+                        # "compression": True,               
+                        # "tileset_version": "1.1",
+                        # "screen_space_error" (float) – Target screen space error (Cesium format only).
+                        # "folder_depth" (int) – Tileset subdivision depth (Cesium format only)
+                #"subdivide_task": True
             }
             # update default params with the input
             default_params.update(kwargs)
-            self.project.chunk.exportPointCloud(path=path+'/point_cloud/point_cloud.las', progress=progress_printer)#,  **default_params)
+            self.project.chunk.exportPointCloud(path=path+'/point_cloud/point_cloud.las', progress=progress_printer,  **default_params)
 
 
 
