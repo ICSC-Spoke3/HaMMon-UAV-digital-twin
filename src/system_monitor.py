e7654a4c749949ec4bfb3087e1072d7c80db0040
Leonardo Pelonero <leo.pelonero@hotmail.it>
2024-06-25 14:46:13 +0200

    code refactor fix values return on SystemMonitor

--- src/system_monitor.py
+++ src/system_monitor.py
@@ -5,6 +5,8 @@ import time
 import threading
 import subprocess
 from src.singleton_meta import SingletonMeta
+from typing import Tuple
+
 
 """
 Sistema per il monitoraggio dei task svolti.
@@ -35,10 +37,10 @@ class SystemMonitor(metaclass=SingletonMeta):
                 # self.logger.info(f'{time.time()}; {cpu_usage}; {cpu_core_usage}; {ram_usage}; {ram_active} GB; {ram_total} GB; {ram_available} GB; {ram_used} GB; {stat["id"]}; {stat["model"]}; {stat["temp"]}; {stat["cpu_usage"]}; {stat["mem_used"]}/{stat["mem_total"]} MB')
             time.sleep(self.time)  # every 30 sec
 
-    def stop(self):
+    def stop(self) -> None:
         self.running = False
     
-    def create_csv(self):
+    def create_csv(self) -> None:
         header = ['Modulo', 'Time', 'CPU usage %', 'Cores usage %', 'RAM usage %', 'RAM active', 'RAM total', 'RAM Available', 'RAM Used']
         gpu_info = self.log_gpu()   # get gpu header info
         header_info = [f'GPUs: {[{entry: info[entry] for entry in ("id", "model", "mem_total")} for info in gpu_info]}']
@@ -47,12 +49,12 @@ class SystemMonitor(metaclass=SingletonMeta):
             writer = csv.writer(file, delimiter=';')
             writer.writerow(header + header_info)
 
-    def parese_dataram(self, ram_memory):
+    def parese_dataram(self, ram_memory) -> float:
         # Convert in GB and exclude the decimal part
         ram_memory = ram_memory / (1024 * 1024 * 1024)
         return ram_memory
 
-    def parse_gpustat(self, gpustat_output):
+    def parse_gpustat(self, gpustat_output) -> list:
         lines = gpustat_output.split('\n')
         stats = []
         for i, line in enumerate(lines):
@@ -72,12 +74,12 @@ class SystemMonitor(metaclass=SingletonMeta):
                 stats.append(stat)
         return stats
 
-    def log_cpu(self):
+    def log_cpu(self) -> Tuple[float, list]:
         cpu_usage = psutil.cpu_percent(interval=None, percpu=False) # avg from last call (delta_t)
         cpu_core_usage = psutil.cpu_percent(interval=None, percpu=True)    # percpu floating for each core cpu
         return cpu_usage, cpu_core_usage
 
-    def log_ram(self):
+    def log_ram(self) -> Tuple[float, float, float, float, float]:
         ram_usage = psutil.virtual_memory().percent
         ram_total = self.parese_dataram(psutil.virtual_memory().total)
         ram_available = self.parese_dataram(psutil.virtual_memory().available)
@@ -85,7 +87,7 @@ class SystemMonitor(metaclass=SingletonMeta):
         ram_used = self.parese_dataram(psutil.virtual_memory().used)
         return ram_usage, ram_total, ram_available, ram_active, ram_used
 
-    def log_gpu(self):
+    def log_gpu(self) -> list:
         result = subprocess.run(['gpustat'], stdout=subprocess.PIPE)
         stats_str = result.stdout.decode('utf-8')
         stats = self.parse_gpustat(stats_str)
