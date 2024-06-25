40575f649bd50b22694b970d9b9acb4317a5a15f
Leonardo Pelonero <leo.pelonero@hotmail.it>
2024-06-25 12:25:04 +0200

    code refactor

--- src/project.py
+++ src/project.py
@@ -5,7 +5,7 @@ from src.singleton_meta import SingletonMeta
 from src.system_monitor import SystemMonitor
     
 class Project(metaclass=SingletonMeta):
-    def __init__(self, project_path: str = None, enable_monitoring: bool = False):
+    def __init__(self, project_path: str = None, enable_monitoring: bool = False) -> None:
         self.project_path = project_path
         self.doc = None
         self.chunk = None
@@ -19,25 +19,25 @@ class Project(metaclass=SingletonMeta):
     def get_project(cls):
         return cls()
 
-    def load_project(self):
+    def load_project(self) -> None:
         self.doc = Metashape.Document()
         self.doc.open(path=self.project_path, read_only=False)
         self.chunk = self.doc.chunk
         print("--Load Project", self.doc.path)
 
-    def new_project(self):
+    def new_project(self) -> None:
         self.doc = Metashape.Document()
         self.save_project(path=self.project_path, version="New project")
         self.chunk = self.doc.addChunk()
         print("--New Project", self.doc.path)
 
     # project version to save
-    def save_project(self, version: str, path: str = None):
+    def save_project(self, version: str, path: str = None) -> None:
         if path == None:
             self.doc.save(version=version)
         else:
             # init project case
             self.doc.save(path=path, version=version)
 
-    def quit_project(self):
+    def quit_project(self) -> None:
         Metashape.app.quit()
