89a4a1ccce97acf62506a5f8ec54bb2de5757ee3
Leonardo Pelonero <leo.pelonero@hotmail.it>
2024-07-16 11:23:16 +0200

    check version testnet

--- testnetwork.py
+++ testnetwork.py
@@ -1,6 +1,11 @@
 import Metashape
 import sys, os
 
+compatible_major_version = "2.1"
+found_major_version = ".".join(Metashape.app.version.split('.')[:2])
+if found_major_version != compatible_major_version:
+    raise Exception("Incompatible Metashape version: {} != {}".format(found_major_version, compatible_major_version))
+
 def find_files(folder, types):
     return [entry.path for entry in os.scandir(folder) if (entry.is_file() and os.path.splitext(entry.name)[1].lower() in types)]
 
