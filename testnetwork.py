c47ff595c921ed3d672e6fa08f70afce623d4827
Leonardo Pelonero <leo.pelonero@hotmail.it>
2024-07-16 10:50:47 +0200

    add testnetwork

---
+++ testnetwork.py
@@ -0,0 +1,61 @@
+import Metashape
+import sys
+
+def find_files(folder, types):
+    return [entry.path for entry in os.scandir(folder) if (entry.is_file() and os.path.splitext(entry.name)[1].lower() in types)]
+
+# Usage: network_processing.py <image_folder> <output_folder>
+image_folder = sys.argv[1]
+output_folder = sys.argv[2]
+
+# Settings
+process_network = True  # TODO: yaml
+network_server = '127.0.0.1' # TODO: è dinamico? richiedo questo da input o yaml settings?
+Metashape.app.settings.network_path = '/mnt/datasets'
+photos = find_files(image_folder, [".jpg", ".jpeg", ".tif", ".tiff"])
+
+# -------New project and import photos-------------
+doc = Metashape.Document()
+doc.save(output_folder + 'progetto.psx')
+chunk = doc.addChunk()
+chunk.addPhotos(photos)
+doc.save()
+# --------------------
+
+print(str(len(chunk.cameras)) + " images loaded")
+
+tasks = []
+# -------primo task-------------
+task = Metashape.Tasks.MatchPhotos()
+task.keypoint_limit = 40000
+task.tiepoint_limit = 10000
+task.generic_preselection = True
+task.reference_preselection = True
+tasks.append(task)
+# --------------------
+
+if process_network:
+    # Rendo tutti i task Network task
+    print("Rendo tutti i task network task")
+    network_tasks = []
+    for task in tasks:
+        if task.target == Metashape.Tasks.DocumentTarget:
+            network_tasks.append(task.toNetworkTask(doc))
+        else:
+            network_tasks.append(task.toNetworkTask(chunk))
+
+    client = Metashape.NetworkClient()
+    client.connect(network_server)
+    batch_id = client.createBatch(doc.path, network_tasks)
+    client.setBatchPaused(batch_id, False)
+
+    print('Processing started, results will be saved to ' + output_folder + '.')
+else:
+    # non è network
+    for task in tasks:
+        if task.target == Metashape.Tasks.DocumentTarget:
+            task.apply(doc) # progress printer
+        else:
+            task.apply(chunk) # progress printer
+
+    print('Processing finished, results saved to ' + output_folder + '.')
