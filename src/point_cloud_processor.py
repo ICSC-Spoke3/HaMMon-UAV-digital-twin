635cf7053f148db49ce7af27a16b647f7a685c24
Leonardo <leo.pelonero@hotmail.it>
2024-10-26 09:45:10 +0200

    mapping build point cloud

--- src/point_cloud_processor.py
+++ src/point_cloud_processor.py
@@ -12,7 +12,29 @@ filter_modes = {
     "Metashape.FilterMode.ModerateFiltering": Metashape.FilterMode.ModerateFiltering,
     "Metashape.ModerateFiltering": Metashape.FilterMode.ModerateFiltering,
     "Metashape.FilterMode.AggressiveFiltering": Metashape.FilterMode.AggressiveFiltering,
-    "Metashape.AggressiveFiltering": Metashape.FilterMode.AggressiveFiltering
+    "Metashape.AggressiveFiltering": Metashape.FilterMode.AggressiveFiltering,
+    "Metashape.DataSource.TiePointsData": Metashape.DataSource.TiePointsData,
+    "Metashape.TiePointsData": Metashape.DataSource.TiePointsData,
+    "Metashape.DataSource.PointCloudData": Metashape.DataSource.PointCloudData,
+    "Metashape.PointCloudData": Metashape.DataSource.PointCloudData,
+    "Metashape.DataSource.ModelData": Metashape.DataSource.ModelData,
+    "Metashape.ModelData": Metashape.DataSource.ModelData,
+    "Metashape.DataSource.TiledModelData": Metashape.DataSource.TiledModelData,
+    "Metashape.TiledModelData": Metashape.DataSource.TiledModelData,
+    "Metashape.DataSource.ElevationData": Metashape.DataSource.ElevationData,
+    "Metashape.ElevationData": Metashape.DataSource.ElevationData,
+    "Metashape.DataSource.OrthomosaicData": Metashape.DataSource.OrthomosaicData,
+    "Metashape.OrthomosaicData": Metashape.DataSource.OrthomosaicData,
+    "Metashape.DataSource.DepthMapsData": Metashape.DataSource.DepthMapsData,
+    "Metashape.DepthMapsData": Metashape.DataSource.DepthMapsData,
+    "Metashape.DataSource.ImagesData": Metashape.DataSource.ImagesData,
+    "Metashape.ImagesData": Metashape.DataSource.ImagesData,
+    "Metashape.DataSource.TrajectoryData": Metashape.DataSource.TrajectoryData,
+    "Metashape.TrajectoryData": Metashape.DataSource.TrajectoryData,
+    "Metashape.DataSource.LaserScansData": Metashape.DataSource.LaserScansData,
+    "Metashape.LaserScansData": Metashape.DataSource.LaserScansData,
+    "Metashape.DataSource.DepthMapsAndLaserScansData": Metashape.DataSource.DepthMapsAndLaserScansData,
+    "Metashape.DepthMapsAndLaserScansData": Metashape.DataSource.DepthMapsAndLaserScansData
 }
 
 def update_existing_keys(dict1, dict2):
