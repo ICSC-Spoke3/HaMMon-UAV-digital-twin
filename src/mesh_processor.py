108b667193871e5e4bbabfd1353e6b50cf503361
Leonardo <leo.pelonero@hotmail.it>
2024-10-26 18:19:50 +0200

    gps fix mesh generation

--- src/mesh_processor.py
+++ src/mesh_processor.py
@@ -7,6 +7,50 @@ import threading
 Determina la creazione della mesh del modello, la texture a partire dalle foto e il tiled model
 """
 
+filter_modes = {
+    "Metashape.SurfaceType.Arbitrary": Metashape.SurfaceType.Arbitrary,
+    "Metashape.Arbitrary": Metashape.SurfaceType.Arbitrary,
+    "Metashape.SurfaceType.HeightField": Metashape.SurfaceType.HeightField,
+    "Metashape.HeightField": Metashape.SurfaceType.HeightField,
+    "Metashape.Interpolation.DisabledInterpolation": Metashape.Interpolation.DisabledInterpolation,
+    "Metashape.DisabledInterpolation": Metashape.Interpolation.DisabledInterpolation,
+    "Metashape.Interpolation.EnabledInterpolation": Metashape.Interpolation.EnabledInterpolation,
+    "Metashape.EnabledInterpolation": Metashape.Interpolation.EnabledInterpolation,
+    "Metashape.Interpolation.Extrapolated": Metashape.Interpolation.Extrapolated,
+    "Metashape.Extrapolated": Metashape.Interpolation.Extrapolated,
+    "Metashape.FaceCount.LowFaceCount": Metashape.FaceCount.LowFaceCount,
+    "Metashape.LowFaceCount": Metashape.FaceCount.LowFaceCount,
+    "Metashape.FaceCount.MediumFaceCount": Metashape.FaceCount.MediumFaceCount,
+    "Metashape.MediumFaceCount": Metashape.FaceCount.MediumFaceCount,
+    "Metashape.FaceCount.HighFaceCount": Metashape.FaceCount.HighFaceCount,
+    "Metashape.HighFaceCount": Metashape.FaceCount.HighFaceCount,
+    "Metashape.FaceCount.CustomFaceCount": Metashape.FaceCount.CustomFaceCount,
+    "Metashape.CustomFaceCount": Metashape.FaceCount.CustomFaceCount,
+    "Metashape.DataSource.TiePointsData": Metashape.DataSource.TiePointsData,
+    "Metashape.TiePointsData": Metashape.DataSource.TiePointsData,
+    "Metashape.DataSource.PointCloudData": Metashape.DataSource.PointCloudData,
+    "Metashape.PointCloudData": Metashape.DataSource.PointCloudData,
+    "Metashape.DataSource.ModelData": Metashape.DataSource.ModelData,
+    "Metashape.ModelData": Metashape.DataSource.ModelData,
+    "Metashape.DataSource.TiledModelData": Metashape.DataSource.TiledModelData,
+    "Metashape.TiledModelData": Metashape.DataSource.TiledModelData,
+    "Metashape.DataSource.ElevationData": Metashape.DataSource.ElevationData,
+    "Metashape.ElevationData": Metashape.DataSource.ElevationData,
+    "Metashape.DataSource.OrthomosaicData": Metashape.DataSource.OrthomosaicData,
+    "Metashape.OrthomosaicData": Metashape.DataSource.OrthomosaicData,
+    "Metashape.DataSource.DepthMapsData": Metashape.DataSource.DepthMapsData,
+    "Metashape.DepthMapsData": Metashape.DataSource.DepthMapsData,
+    "Metashape.DataSource.ImagesData": Metashape.DataSource.ImagesData,
+    "Metashape.ImagesData": Metashape.DataSource.ImagesData,
+    "Metashape.DataSource.TrajectoryData": Metashape.DataSource.TrajectoryData,
+    "Metashape.TrajectoryData": Metashape.DataSource.TrajectoryData,
+    "Metashape.DataSource.LaserScansData": Metashape.DataSource.LaserScansData,
+    "Metashape.LaserScansData": Metashape.DataSource.LaserScansData,
+    "Metashape.DataSource.DepthMapsAndLaserScansData": Metashape.DataSource.DepthMapsAndLaserScansData,
+    "Metashape.DepthMapsAndLaserScansData": Metashape.DataSource.DepthMapsAndLaserScansData,
+
+}
+
 class MeshProcessor:
 
     def __init__(self) -> None:
@@ -25,13 +69,37 @@ class MeshProcessor:
             'vertex_confidence': True,
             'keep_depth': True,
             'split_in_blocks': False,
-            'blocks_crs': Metashape.CoordinateSystem("WGS 84"),
             'blocks_size': 250,
             'build_texture': True,
             'subdivide_task': True
         }
         # update default params with the input
         default_params.update(kwargs)
+        if 'surface_type' in kwargs:
+            try:
+                surface_type = filter_modes.get(kwargs['surface_type'], Metashape.Arbitrary)
+            except AttributeError:
+                print(f"Note: '{kwargs['surface_type']}' is not valid on Metashape.")
+            default_params['surface_type'] = surface_type  # surface_type updated correctly
+        if 'source_data' in kwargs:
+            try:
+                source_data = filter_modes.get(kwargs['source_data'], Metashape.DepthMapsData)
+            except AttributeError:
+                print(f"Note: '{kwargs['source_data']}' is not valid on Metashape.")
+            default_params['source_data'] = source_data  # source_data updated correctly
+        if 'interpolation' in kwargs:
+            try:
+                interpolation = filter_modes.get(kwargs['interpolation'], Metashape.EnabledInterpolation)
+            except AttributeError:
+                print(f"Note: '{kwargs['interpolation']}' is not valid on Metashape.")
+            default_params['interpolation'] = interpolation  # interpolation updated correctly
+        if 'face_count' in kwargs:
+            try:
+                face_count = filter_modes.get(kwargs['face_count'], Metashape.HighFaceCount)
+            except AttributeError:
+                print(f"Note: '{kwargs['face_count']}' is not valid on Metashape.")
+            default_params['face_count'] = face_count  # face_count updated correctly
+
         if self.project.monitoring is not None:
             thread = threading.Thread(target=self.project.monitoring.start, args=('buildModel',))
             thread.start()
