33548f8115b441dc584f17cec761fac957b57c64
Leonardo <leo.pelonero@hotmail.it>
2024-11-23 11:55:57 +0100

    add export_worker_info

---
+++ export_worker_info.py
@@ -0,0 +1,15 @@
+import Metashape
+import os
+import json
+
+client = Metashape.NetworkClient()
+client.connect(os.getenv("METASHAPE_SERVER"))
+
+# Cicla attraverso i worker e prendi il worker_id
+for worker in client.workerList()['workers']:
+    worker_id = worker['worker_id']
+    file_name = f'worker_{worker_id}.json'
+    worker_info = client.workerInfo(worker_id)
+    with open(file_name, 'r') as json_file:
+        json.dump(worker_info, json_file, indent=4)
+
